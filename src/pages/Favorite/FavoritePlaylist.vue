5324665c4ff1f6b4a00df86d03a69219104a7c1a
LiangYin <liangyin17@outlook.com>
2024-01-18 21:46:51 +0800

    取消收藏

--- src/pages/Favorite/FavoritePlaylist.vue
+++ src/pages/Favorite/FavoritePlaylist.vue
@@ -16,6 +16,9 @@
                     }" v-show="!playlistData[item].loading">{{ playlistData[item].name }}</RouterLink>
                     <span v-show="playlistData[item].loading">加载中</span>
                 </p>
+                <div class="Mcontrol">
+                    <button @click="favoriteStore.deleteFavoritePlaylist(item.id)">取消收藏</button>
+                </div>
             </div>
             <div v-show="favoritePlaylist.length == 0" class="noPlaylist">没有歌单！</div>
         </div>
